afa7ddbfd63379fa5846960fd63d0720f76a01c9
Houmine-Rayyane <houmrayyane@gmail.com>
2024-02-22 23:58:39 -0500

    Fixed some issues, score display and replaying the game

--- main.py
+++ main.py
@@ -16,13 +16,12 @@ Fps = 60
 clk = pygame.time.Clock()
 #game background
 space_surf = pygame.image.load('pics/space.png')
-scoreact_font = pygame.font.Font('fonts/gamefont.ttf',20)
-scoreinact_font = pygame.font.Font('fonts/gamefont.ttf',40)
+txt_font = pygame.font.Font('fonts/gamefont.ttf',20)
 
 #Allien hunter
-allien_hunter = pygame.image.load('pics/Hero.png').convert_alpha()
-ah_rect = allien_hunter.get_rect(midbottom =(200,600))
-ah_mask = pygame.mask.from_surface(allien_hunter)
+spaceship = pygame.image.load('pics/spaceship.png').convert_alpha()
+ah_rect = spaceship.get_rect(midbottom =(200,600))
+ah_mask = pygame.mask.from_surface(spaceship)
 
 #Enemies
 alien = pygame.image.load('pics/alien.png').convert_alpha()
@@ -43,31 +42,38 @@ health = 5
 
 #Bullets 
 bullets = []
-
-# Hitting the alliens
-#A1_HIT = pygame.USEREVENT + 1
-#A2_HIT = pygame.USEREVENT + 2
-
 bullet_sound = pygame.mixer.Sound('audio/bulletsound.mp3')
+
 game_active = True
-        
+
+rotation_angle = 0
+score = 0 
+
 def handle_aliens(aliens_list,almasks_list):
     # Spawn new aliens periodically
-    x = randint(0, WIDTH - alien.get_width())  # Random X position
-    y = 60  # Fixed Y position
+    x = randint(0, WIDTH - alien.get_width()) 
+    y = 60  
     new_alien_rect = alien.get_rect(midtop=(x, y))
+    #prevent two alliens from respawning at the same position
+    while any(new_alien_rect.colliderect(existing_alien) for existing_alien in aliens_list):
+        x = randint(0, WIDTH - alien.get_width()) 
+        y = 60  
+        new_alien_rect = alien.get_rect(midtop=(x, y))
+        
     aliens_list.append(new_alien_rect)
     almasks_list.append(pygame.mask.from_surface(alien))
 
-def shoot(bullets):
+def shoot(bullets,score):
     for bullet in bullets:
         bullet.y -=15
         #Check for aliens/bullet collisions
         for alien_rect, alien_mask in zip(aliens_list,almasks_list):
             if alien_rect.collidepoint(bullet.x, bullet.y):
+                score+=1
                 aliens_list.remove(alien_rect)
                 almasks_list.remove(alien_mask)
                 bullets.remove(bullet)
+    return score
                 
                 
 def move_spaceship(keys_pressed, ah_rect):
@@ -76,7 +82,9 @@ def move_spaceship(keys_pressed, ah_rect):
     elif keys_pressed[pygame.K_RIGHT] and ah_rect.x  < WIDTH - 105  :
       ah_rect.x +=5  
       
+    
 while True:
+    pygame.event.pump()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -84,6 +92,7 @@ while True:
         #Shooting event   
         if event.type==pygame.KEYDOWN :
             if event.key == pygame.K_SPACE:
+                game_active = True
                 bullet_sound.play()
                 bullet=pygame.Rect(ah_rect.x + ah_rect.width//2 -2,ah_rect.y, 5 , 10)
                 bullets.append(bullet)
@@ -94,7 +103,7 @@ while True:
         
     if game_active:
         screen.blit(space_surf,(0,0))
-        screen.blit(allien_hunter, ah_rect)
+        screen.blit(spaceship, ah_rect)
         pygame.draw.rect(screen,'black', border)
         
         #Drawing hearts at the top of the window
@@ -116,12 +125,33 @@ while True:
         #Spaceship bullets
         for bullet in bullets:
             pygame.draw.rect(screen,'red',bullet)
+        
+        score = shoot(bullets,score)
     else:
+        #Resetting the health and bullets
+        health = 5
+        bullets.clear()
+        #Background
         screen.blit(space_surf,(0,0))
-                       
+        #Displaying the score
+        score_txt = txt_font.render(f'Your score :{score}',False,'White')
+        score_rect = score_txt.get_rect(center = (200,150))
+        screen.blit(score_txt, score_rect)
+        #Displaying the message 
+        msg_txt = txt_font.render(f'Press Space bar',False,'White')
+        msg_rect = score_txt.get_rect(center = (180,450))
+        screen.blit(msg_txt, msg_rect)
+        #rotating spaceship
+        rotation_angle+=1
+        if rotation_angle >= 360:
+            rotation_angle = 0
+        rot_spaceship = pygame.transform.rotate(spaceship, rotation_angle)  
+        rot_rect = rot_spaceship.get_rect(center =(WIDTH // 2, HEIGHT // 2))  
+        screen.blit(rot_spaceship, rot_rect.topleft)  
+        pygame.display.flip()      
+        
      
     keys_pressed= pygame.key.get_pressed()        
     move_spaceship(keys_pressed, ah_rect)     
-    shoot(bullets)
     pygame.display.update()
     clk.tick(Fps)
